e137340425133195f4f312446e5e8dde6a77a6b8
Regina <regina.p@digidirect.com.au>
2026-05-21 11:51:50 +0800

    Test scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
+mix.disableNotifications();
 
-mix.sass('src/sass/sections/product-reels-slideshow.scss','assets/product-reels-slideshow.css')
+mix.sass('src/sass/sections/header.scss','assets/header.css')
     .js('src/js/components/collection-categories.js','assets/collection-categories.js')
